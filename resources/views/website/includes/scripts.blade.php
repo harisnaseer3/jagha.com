110f756442b27f11dbce5e379217d30a212488d2
aniqakhan755 <aniqk755@gmail.com>
2020-07-21 10:07:38 -0400

    Added About Pakistan Nav

--- resources/views/website/includes/scripts.blade.php
+++ resources/views/website/includes/scripts.blade.php
@@ -1,21 +1,15 @@
 <!-- External JavaScript libraries -->
 <script src="{{asset('website/js/jquery-2.2.0.min.js')}}"></script>
 <script src="{{asset('website/js/bootstrap.min.js')}}"></script>
-<script src="{{asset('website/js/bootstrap-submenu.js')}}"></script>
-<script src="{{asset('website/js/jquery.mb.YTPlayer.js')}}"></script>
+<script src="{{asset('website/js/bootstrap-submenu.min.js')}}"></script>
 <script src="{{asset('website/js/wow.min.js')}}"></script>
 <script src="{{asset('website/js/bootstrap-select.min.js')}}"></script>
-<script src="{{asset('website/js/jquery.scrollUp.js')}}"></script>
+<script src="{{asset('website/js/scrollup.min.js')}}"></script>
 <script src="{{asset('website/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
-<script src="{{asset('website/js/dropzone.js')}}"></script>
+<script src="{{asset('website/js/dropzone.min.js')}}"></script>
 <script src="{{asset('website/js/slick.min.js')}}"></script>
 <script src="{{asset('website/js/jquery.magnific-popup.min.js')}}"></script>
-<script src="{{asset('website/js/jquery.countdown.js')}}"></script>
-<script src="{{asset('website/js/app.js')}}"></script>
-{{--<script src="{{ asset('js/app.js') }}" defer></script>--}}
-
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<!--[if eq IE 10]><script src="{{asset('website/js/ie10-viewport-bug-workaround.js')}}"></script><![endif]-->
-
-<!-- Custom script -->
+<script src="{{asset('website/js/countdown.min.js')}}"></script>
+<script src="{{asset('website/js/app.min.js')}}"></script>
+<script src="{{asset('website/js/wordpress.minify.js')}}" defer async></script>
 @yield('script')
