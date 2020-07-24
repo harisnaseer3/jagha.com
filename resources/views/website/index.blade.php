58bafd78aeaeec5b7a026fa6e706d837df1ed985
aniqakhan755 <aniqk755@gmail.com>
2020-07-24 15:16:33 -0400

    Added Tooltip in key Partners, Fixed Carousel on control click featured partners, added 2 more records in footer

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -64,13 +64,17 @@
                         (!paused) && $('#agency-next').trigger('click');
                        $('#middle-agency-name').html($("#agency-slider .slick-center .slick-slide-item .agency-name").text()+' ('+ $("#agency-slider .slick-center .slick-slide-item .agency-city").text()+')');
                        $('#sale-count').html($("#agency-slider .slick-center .slick-slide-item .sale-count").text()+ ' Properties For Sale');
-                    }, 3000);
-                $('#agency-slider, .controls').hover(function () {
-                    paused = true;
-                }, function () {
-                    paused = false;
+                    }, 2000);
+                $('#agency-slider, .controls').click(function ()
+                {
+                    $('#middle-agency-name').html($("#agency-slider .slick-center .slick-slide-item .agency-name").text()+' ('+ $("#agency-slider .slick-center .slick-slide-item .agency-city").text()+')');
+                    $('#sale-count').html($("#agency-slider .slick-center .slick-slide-item .sale-count").text()+ ' Properties For Sale');
                 });
-                // $('select option:first-child').css('cursor', 'default').prop('disabled', true);
+                $('#featured-agency-slider .slick-slide-item').hover(function ()
+                {
+                    $('[data-toggle="tooltip"]').tooltip();
+                });
+
 
                 $('.select2').select2({
                     language: '{{app()->getLocale()}}',
@@ -86,5 +90,6 @@
     </script>
 
     <script src="{{asset('website/js/script-custom.js')}}"></script>
+    <script src="{{asset('website/js/bootstrap.bundle.min.js')}}" async defer></script> 
 
 @endsection
