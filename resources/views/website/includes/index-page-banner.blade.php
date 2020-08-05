f92007157f2a567abde9d4d0c9d264666272450f
aniqakhan755 <aniqk755@gmail.com>
2020-08-05 08:36:11 -0400

    Popular Cities Carousel

--- resources/views/website/includes/index-page-banner.blade.php
+++ resources/views/website/includes/index-page-banner.blade.php
@@ -160,17 +160,67 @@
                                     </a>
                                 </div>
                             </div>
-                            {{--                            <div class="inline-search-area none-992 show-advance-search-options">--}}
-                            {{--                                <div class="search-options-btn-area" style="margin-top:8px !important;">--}}
-                            {{--                                    <a class="btn btn-outline-primary reset-search-btn" data-toggle="modal" data-target="#modalCart">--}}
-                            {{--                                        <span class="reset-search">Change Area Unit</span>--}}
-                            {{--                                    </a>--}}
-                            {{--                                </div>--}}
-                            {{--                            </div>--}}
+
                         </div>
+                        <div class="popular-cities">
+    <div class="card">
+    <div class="card-header">
+    <h6>Popular Cities</h6>
+    </div>
+        <!-- <div class="slider"></div> -->
+        <div class="slick-slider-area" id="featured-agency-slider">
+            <div class="row slick-carousel" id="feature-agency-row-1" data-cycle-fx="carousel" data-cycle-timeout="0" data-cycle-next="slick-next" data-cycle-prev="slick-prev"
+                 data-cycle-carousel-horizontal="true"
+                 data-slick='{"slidesToShow": 1, "rows":1,"responsive":[{"breakpoint": 1024,"settings":{"slidesToShow": 3}}, {"breakpoint": 768,"settings":{"slidesToShow": 1}}]}'>
+                @foreach($key_agencies as $agency)
+                    <div class="slick-slide-item" aria-label="key agency">
+                    @if($agency->logo !== null)
+                           
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h4><a href="#">{{\Illuminate\Support\Str::limit($agency->title, 25, $end='..')}}</a></h4>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-4">
+                                        <img src="{{asset('thumbnails/agency_logos/'.explode('.',$agency->logo)[0].'-100x100'.'.webp')}}" alt="{{$agency->title}}" class="img-fluid featured-agency-image"
+
+                                             style="height:53px; width: 60px ;">
+                                    </div>
+                                    <div class="col-8">
+                                        <p><i class="fa fa-map-marker mr-1 mt-1 fa-2x" aria-hidden="true"></i>{{implode(", ", json_decode($agency->city))}}</p>
+                                        <p>
+                                            @if($agency->phone == null || preg_match('/\+92$/', $agency->phone))
+                                            @else
+                                                <i class="fa fa-phone mr-1 fa-2x" aria-hidden="true"></i>{{explode('-+92',$agency->phone)[0]}}
+                                            @endif
+                                        </p>
+                                    </div>
+                                </div>
+                           
+                        @else
+                            <img src="{{asset('storage/agency_logos/'.'img256by256_1588397752.jpg')}}" alt="{{$agency->title}}"
+                                 title='<div class="row p-2"><div class="col-md-12"><p class="color-white mb-2">{{$agency->title}}</p><p class="color-white"> <i class="fa fa-map-marker mr-2" aria-hidden="true"></i>{{implode(", ", json_decode($agency->city))}}</p><p class="color-white mb-2"><i class="fa fa-phone mr-2" aria-hidden="true"></i>{{$agency->phone}}</p></div></div>'
+                                 data-toggle="tooltip" data-html="true" data-placement="top" class="img-fluid featured-agency-image" style="height:53px; width: 53px ;">
+                        @endif
+                    </div>
+                @endforeach
+            </div>
+            <div class="controls">
+                <div class="slick-prev slick-arrow-buton top-style-prev" id="featured-agency-prev">
+                    <i class="fas fa-angle-left"></i>
+                </div>
+                <div class="slick-next slick-arrow-buton top-style-next" id="featured-agency-next">
+                    <i class="fas fa-angle-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
                     </div>
                 </div>
             </div>
+     
         </div>
     </div>
 </div>
