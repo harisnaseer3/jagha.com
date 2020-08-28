fd94ea43131f4377a5f44f3261554c9e4329ca40
aniqakhan755 <aniqk755@gmail.com>
2020-08-28 09:31:55 -0400

    Added Icons and Corrected Fonts

--- resources/views/website/includes/property_counter.blade.php
+++ resources/views/website/includes/property_counter.blade.php
@@ -43,16 +43,18 @@
 <section class="wow fadeIn animated counters" style="visibility: visible; animation-name: fadeIn;">
     <div class="container">
         <div class="row">
-            <!-- counter -->
-            <div class="col-md-3 col-sm-6 bottom-margin text-center counter-section wow fadeInUp sm-margin-bottom-ten animated" data-wow-duration="300ms" style="visibility: visible; animation-duration: 300ms; animation-name: fadeInUp;"> <i class="fa fa-beer medium-icon"></i> <span id="anim-number-pizza" class="counter-number"></span> <span class="timer counter alt-font appear" data-to="980" data-speed="7000">2800</span>
-                <p class="counter-title">Beer Ordered</p>
+        <div class="col-md-3 col-sm-6 bottom-margin text-center counter-section wow fadeInUp sm-margin-bottom-ten animated" data-wow-duration="300ms" style="visibility: visible; animation-duration: 300ms; animation-name: fadeInUp;"> <i class="fa fa-beer medium-icon"></i> <span id="anim-number-pizza" class="counter-number"></span> <span class="timer counter alt-font appear" data-to="{{$total_count[0]->property_count}}" data-speed="7000">{{$total_count[0]->property_count}}</span>
+                <p class="counter-title">Properties To Sale</p>
             </div> <!-- end counter -->
             <!-- counter -->
-            <div class="col-md-3 col-sm-6 bottom-margin text-center counter-section wow fadeInUp sm-margin-bottom-ten animated" data-wow-duration="600ms" style="visibility: visible; animation-duration: 600ms; animation-name: fadeInUp;"> <i class="fa fa-heart medium-icon"></i> <span class="timer counter alt-font appear" data-to="980" data-speed="7000">980</span> <span class="counter-title">Happy Clients</span> </div> <!-- end counter -->
+            <div class="col-md-3  col-sm-6 bottom-margin text-center counter-section wow fadeInUp sm-margin-bottom-ten animated" data-wow-duration="300ms" style="visibility: visible; animation-duration: 300ms; animation-name: fadeInUp;"> <i class="fa fa-beer medium-icon"></i> <span id="anim-number-pizza" class="counter-number"></span> <span class="timer counter alt-font appear" data-to="{{$total_count[0]->property_count}}" data-speed="7000">{{$total_count[0]->property_count}}</span>
+                <p class="counter-title">Properties To Rent</p>
+            </div> <!-- end counter -->
             <!-- counter -->
-            <div class="col-md-3 col-sm-6 bottom-margin-small text-center counter-section wow fadeInUp xs-margin-bottom-ten animated" data-wow-duration="900ms" style="visibility: visible; animation-duration: 900ms; animation-name: fadeInUp;"> <i class="fa fa-anchor medium-icon"></i> <span class="timer counter alt-font appear" data-to="810" data-speed="7000">810</span> <span class="counter-title">Projects Completed</span> </div> <!-- end counter -->
+            <div class="col-md-3  col-sm-6 bottom-margin text-center counter-section wow fadeInUp sm-margin-bottom-ten animated" data-wow-duration="600ms" style="visibility: visible; animation-duration: 600ms; animation-name: fadeInUp;"> <i class="fa fa-handshake medium-icon"></i> <span class="timer counter alt-font appear" data-to="{{$total_count[0]->agency_count}}" data-speed="7000">{{$total_count[0]->agency_count}}</span> <span class="counter-title">Partners</span> </div> <!-- end counter -->
             <!-- counter -->
-            <div class="col-md-3 col-sm-6 text-center counter-section wow fadeInUp animated" data-wow-duration="1200ms" style="visibility: visible; animation-duration: 1200ms; animation-name: fadeInUp;"> <i class="fa fa-user medium-icon"></i> <span class="timer counter alt-font appear" data-to="600" data-speed="7000">600</span> <span class="counter-title">Clients Served</span> </div> <!-- end counter -->
+            <div class="col-md-3  col-sm-6 bottom-margin-small text-center counter-section wow fadeInUp xs-margin-bottom-ten animated" data-wow-duration="900ms" style="visibility: visible; animation-duration: 900ms; animation-name: fadeInUp;"> <i class="fas fa-city medium-icon"></i> <span class="timer counter alt-font appear" data-to="{{$total_count[0]->city_count}}" data-speed="7000">{{$total_count[0]->city_count}}</span> <span class="counter-title">Cities</span> </div> <!-- end counter -->
+
         </div>
     </div>
 </section>
