d61be5c7c9efcd339f6d508ee487dfa2bc0e7b26
coder's_corner <ridafatima477@gmail.com>
2020-09-17 00:28:41 +0500

    complete sending mail to new user

---
+++ database/migrations/2020_09_16_173422_create_user_invites_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInvitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_invites', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        DB::statement('ALTER TABLE `user_invites`
+            CHANGE `created_at` `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+            CHANGE `updated_at` `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL DEFAULT NULL');
+}
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_invites');
+    }
+}
