1ffc4270b7f7975b1398976b39e5698519fa4848
aniqakhan755 <aniqk755@gmail.com>
2020-10-05 13:06:27 -0400

    Sub level Permissions added and commented

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -26,6 +26,28 @@ class PermissionsTableSeeder extends Seeder
         Permission::create(['guard_name' => 'admin', 'name' => 'Manage Property']);
         Permission::create(['guard_name' => 'admin', 'name' => 'Manage Agency']);
         Permission::create(['guard_name' => 'admin', 'name' => 'Manage Packages']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Manage Active Properties']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Manage Edited Properties']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Manage Pending Properties']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Manage Expired Properties']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Manage Deleted Properties']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Manage Rejected Properties']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Manage Sold Properties']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Edit Property']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Delete Property']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Update Property Status']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Manage Verified Agencies']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Manage Pending Agencies']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Manage Expired Agencies']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Manage Rejected Agencies']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Manage Deleted Agencies']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Add New Agency']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Add Agency Users']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Edit Agency']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Delete Agency']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Update Agency Status']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Add Admin']);
+//        Permission::create(['guard_name' => 'admin', 'name' => 'Activate/Deactivate Admin']);
 
 
 
