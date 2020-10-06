97e955651fc9f98e425f5c79520b283a66ee8995
coder's_corner <ridafatima477@gmail.com>
2020-10-06 20:51:08 +0500

    working on datatabels

--- resources/views/website/admin-pages/listings.blade.php
+++ resources/views/website/admin-pages/listings.blade.php
@@ -6,6 +6,8 @@
 @section('css_library')
     <link rel="stylesheet" type="text/css" href="{{asset('website/css/custom-dashboard-style.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('website/css/custom.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('website/css/datatables.min.css')}}">
+
 @endsection
 
 @section('content')
@@ -31,7 +33,8 @@
                                                 <h6>{{ucwords($option)}} Listings</h6>
                                                 <div class="my-4">
                                                     <div class="table-responsive">
-                                                        <table class="table table-sm table-bordered">
+{{--                                                        <table class="table table-sm table-bordered">--}}
+                                                            <table id="property-log" class="display" style="width: 100%">
                                                             <thead class="theme-blue text-white">
                                                             <tr>
                                                                 <td>ID</td>
@@ -175,9 +178,13 @@
 @endsection
 
 @section('script')
+    <script type="text/javascript" charset="utf8" src="{{asset('website/js/datatables.min.js')}}"></script>
+
     <script>
         (function ($) {
             $(document).ready(function () {
+                $('#property-log').DataTable();
+
                 $('#delete').on('show.bs.modal', function (event) {
                     let record_id = $(event.relatedTarget).data('record-id');
                     $(this).find('.modal-body #record-id').val(record_id);
