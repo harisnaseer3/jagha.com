1efb3734b8f8cff841c049fc1fb60a85aa69db3b
aniqakhan755 <aniqk755@gmail.com>
2020-10-07 12:02:27 -0400

    Update Admin Password if user password is updated

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -1,12 +1,13 @@
 <?php
+
 namespace App\Models;
 
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Builder;
-
 
 
 /**
@@ -16,34 +17,42 @@ use Illuminate\Database\Eloquent\Builder;
 class Admin extends Authenticatable
 {
     use HasRoles;
-/**
-* The attributes that are mass assignable.
-*
-* @var array
-*/
-protected $fillable = [
-'name', 'email', 'password'
-];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password'
+    ];
 
 
-/**
-* The attributes excluded from the model's JSON form.
-*
-* @var array
-*/
-protected $hidden = [
-'password', 'remember_token',
-];
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 
     public static function getAllAdmins()
     {
         $condition = ['is_super' => '0'];
-        return (new Admin)->select(['id', 'name', 'email','is_active'])->where($condition)->with('roles:name')->get();
+        return (new Admin)->select(['id', 'name', 'email', 'is_active'])->where($condition)->with('roles:name')->get();
     }
+
     public static function getAdminById($id)
     {
         return (new Admin)->select(['id', 'name', 'email'])->findOrFail($id);
     }
+
+    public static function getAdminByEmail($email)
+    {
+        return (new Admin)->where('email', $email)->first();
+    }
+
     public static function updateAdmin($object, $id)
     {
         $current_admin = (new Admin)->findOrFail($id);
@@ -56,16 +65,23 @@ protected $hidden = [
         //updating admin role
         $current_admin->syncRoles($object['role']);
     }
+
+    public static function updateAdminPassword($id, $password)
+    {
+      return  DB::table('admins')
+            ->where('id',$id)
+            ->update(['password' => $password]);
+      
+
+    }
+
     public static function destroy($id)
     {
         $current_admin = (new Admin)->find($id);
         try {
-            if($current_admin->is_active === '1')
-            {
+            if ($current_admin->is_active === '1') {
                 $current_admin->is_active = '0';
-            }
-            elseif($current_admin->is_active === '0')
-            {
+            } elseif ($current_admin->is_active === '0') {
                 $current_admin->is_active = '1';
             }
 
@@ -74,6 +90,7 @@ protected $hidden = [
         } catch (\Exception $e) {
         }
     }
+
     public function getAdminsByRole($role)
     {
         return (new Admin)
@@ -84,6 +101,7 @@ protected $hidden = [
             ->pluck('name', 'email')
             ->toArray();
     }
+
     public function getPermissionsByRole($role_name)
     {
         return Role::findByName($role_name)->permissions->map->only('name');
