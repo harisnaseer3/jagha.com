f379404361b48875739c621b2809ad89a25e52e7
aniqakhan755 <aniqk755@gmail.com>
2020-10-23 11:27:15 -0400

    Defering js files

--- resources/views/website/includes/scripts.blade.php
+++ resources/views/website/includes/scripts.blade.php
@@ -1,14 +1,15 @@
 <!-- External JavaScript libraries -->
 <script src="{{asset('website/js/jquery-2.2.0.min.js')}}"></script>
 <script src="{{asset('website/js/bootstrap.min.js')}}"></script>
-<script src="{{asset('website/js/bootstrap-submenu.min.js')}}"></script>
-<script src="{{asset('website/js/wow.min.js')}}"></script>
-<script src="{{asset('website/js/bootstrap-select.min.js')}}"></script>
-<script src="{{asset('website/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
-<script src="{{asset('website/js/dropzone.min.js')}}"></script>
-<script src="{{asset('website/js/slick.min.js')}}"></script>
-<script src="{{asset('website/js/jquery.magnific-popup.min.js')}}"></script>
-<script src="{{asset('website/js/countdown.min.js')}}"></script>
-<script src="{{asset('website/js/app.js')}}"></script>
+<script src="{{asset('website/js/bootstrap-submenu.min.js')}}" defer></script>
+<script src="{{asset('website/js/wow.min.js')}}" defer></script>
+<script src="{{asset('website/js/bootstrap-select.min.js')}}" defer></script>
+<script src="{{asset('website/js/jquery.mCustomScrollbar.concat.min.js')}}" defer ></script>
+<script src="{{asset('website/js/dropzone.min.js')}}" defer></script>
+<script src="{{asset('website/js/slick.min.js')}}" defer></script>
+<script src="{{asset('website/js/jquery.magnific-popup.min.js')}}" defer></script>
+<script src="{{asset('website/js/countdown.min.js')}}" defer></script>
+<script src="{{asset('website/js/app.js')}}" defer></script>
 <script src="{{asset('website/js/wordpress.minify.js')}}" defer async></script>
+<script src="{{asset('website/js/bootstrap.bundle.min.js')}}" defer></script>
 @yield('script')
