c6a52afda1f28a706881cd8b287782b76d3cea54
aniqakhan755 <aniqk755@gmail.com>
2020-11-06 08:51:55 -0500

    Locations Wise Listing

---
+++ resources/views/website/includes/locations_count_card.blade.php
@@ -0,0 +1,19 @@
+<div class="card mb-3">
+    <div class="card-header bg-white">
+        <h2 class="pull-left all-cities-header title"></h2>
+        <span class="btn btn-sm btn-outline pull-right clickable" id="close-icon" data-effect="fadeOut">close</span>
+    </div>
+    <div class="card-body" id="locations-card">
+        <div class="row">
+
+                <div class="col-sm-3 my-2">
+                    <a href=""
+                       title="agencies in"
+                       class="breadcrumb-link">
+
+                    </a>
+                </div>
+
+        </div>
+    </div>
+</div>
