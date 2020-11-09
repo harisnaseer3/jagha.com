788365a0669a217cfc89e81cd48af125de6bf13b
aniqakhan755 <aniqk755@gmail.com>
2020-11-09 13:28:32 -0500

    Updated Footer Social Links

--- resources/views/website/includes/footer.blade.php
+++ resources/views/website/includes/footer.blade.php
@@ -22,10 +22,10 @@
                             <div class="footer-divider"></div>
                             <div class="text-white mt-2"> Join us on Social</div>
                             <div class="color-white mt-2">
-                                <a class="mr-2" href="https://www.facebook.com/aboutpk1" target="_blank" title="Visit About Pakistan Facebook"><i class="fab fa-facebook-f"></i></a>
-                                <a class="mr-2" href="https://twitter.com/aboutpk_" target="_blank" title="Visit About Pakistan Twitter"><i class="fab fa-twitter"></i> </a>
-                                <a class="mr-2" href="https://www.linkedin.com/company/aboutpakistan" target="_blank" title="Visit About Pakistan Linkedin"><i class="fab fa-linkedin in"></i> </a>
-                                <a class="mr-2" href="https://www.instagram.com/aboutpk1/?igshid=19bpcgb38304h" target="_blank" title="Visit About Pakistan Instagram"><i class="fab fa-instagram"></i>
+                                <a class="mr-2" href="https://www.facebook.com/aboutpkofficial" target="_blank" title="Visit About Pakistan Facebook"><i class="fab fa-facebook-f"></i></a>
+                                <a class="mr-2" href="https://twitter.com/aboutpkofficial" target="_blank" title="Visit About Pakistan Twitter"><i class="fab fa-twitter"></i> </a>
+                                <a class="mr-2" href="https://www.linkedin.com/company/aboutpkofficial" target="_blank" title="Visit About Pakistan Linkedin"><i class="fab fa-linkedin in"></i> </a>
+                                <a class="mr-2" href="https://www.instagram.com/aboutpkofficial/" target="_blank" title="Visit About Pakistan Instagram"><i class="fab fa-instagram"></i>
                                 </a>
                                 <a class="mr-2" href="https://www.youtube.com/channel/UCfarVSSCib1eZ6sjFR3-gnA" target="_blank" title="Visit About Pakistan Youtube"><i class="fab fa-youtube"></i> </a>
                             </div><!--foot-soc-->
@@ -77,16 +77,16 @@
                     <div class="foot-menu relative">
                         <div class="menu-main-menu-container">
                             <ul id="menu-main-menu-2" class="menu">
-                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2457 page-font"><a href="/index.php">Home</a></li>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2457 page-font"><a href="https://www.aboutpakistan.com">Home</a></li>
                                 <li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-has-children menu-item-2519 page-font">
-                                    <a href="/blog" aria-current="page">Blog</a>
+                                    <a href="https://www.aboutpakistan.com/blog" aria-current="page">Blog</a>
                                 </li>
-                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-10 page-font"><a href="/news">News</a>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-10 page-font"><a href="https://www.aboutpakistan.com/news">News</a>
                                 </li>
-                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2515 page-font"><a href="/medical">Medical</a></li>
-                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2516 page-font"><a href="/mobile-packages">Mobile Packages</a></li>
-                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2516 page-font"><a href="/about-us.php">About Us</a></li>
-                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2516 page-font"><a href="/contact-us.php">Contact Us</a></li>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2515 page-font"><a href="https://www.aboutpakistan.com/medical">Medical</a></li>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2516 page-font"><a href="https://www.aboutpakistan.com/mobile-packages">Mobile Packages</a></li>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2516 page-font"><a href="https://www.aboutpakistan.com/about-us.php">About Us</a></li>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2516 page-font"><a href="https://www.aboutpakistan.com/contact-us.php">Contact Us</a></li>
 
                             </ul>
                         </div>
