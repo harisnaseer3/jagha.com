8bc28d4e3839bfb1d3830fa52b618ad6c474a434
fatimarida <ridafatima477@gmail.com>
2020-11-27 13:22:40 +0500

    added international number on register page

--- website/js/register-user.js
+++ website/js/register-user.js
@@ -11,16 +11,52 @@
     $.validator.addMethod("checkspecialchr", function (value) {
         return /[#?!@$%^&*-]/.test(value);
     });
-    $.validator.addMethod("checkcellnum", function (value) {
-        return /^3\d{2}[\s.-]?\d{7}$/.test(value) || /^03\d{2}[\s.-]?\d{7}$/.test(value);
-    });
+    // $.validator.addMethod("checkcellnum", function (value) {
+    //     return /^3\d{2}[\s.-]?\d{7}$/.test(value) || /^03\d{2}[\s.-]?\d{7}$/.test(value);
+    // });
 
 
     $(document).ready(function () {
 
-        $("input[name='cell']").keyup(function () {
-            $(this).val($(this).val().replace(/^(\d{1})(\d+)$/, "+92-$2"));
+        let input = document.querySelector("#cell");
+        var errorMsg = document.querySelector("#error-msg"),
+            validMsg = document.querySelector("#valid-msg");
+        var errorMap = ["Invalid number", "Invalid country code", "Too short", "Too long", "Invalid number"];
+
+        var ag_iti_cell = window.intlTelInput(document.querySelector('#cell'), {
+            preferredCountries: ["pk"],
+            preventInvalidNumbers: true,
+            separateDialCode: true,
+            numberType: "MOBILE",
+            // hiddenInput: "mobile",
+            utilsScript: "/../../plugins/intl-tel-input/js/utils.js?1603274336113"
         });
+        var reset = function () {
+            input.classList.remove("error");
+            errorMsg.innerHTML = "";
+            errorMsg.classList.add("hide");
+            validMsg.classList.add("hide");
+        };
+        input.addEventListener('blur', function () {
+            reset();
+            if (input.value.trim()) {
+                if (ag_iti_cell.isValidNumber()) {
+                    $('[name=mobile]').val(ag_iti_cell.getNumber());
+                    validMsg.classList.remove("hide");
+                    $('#mobile-error').hide();
+                } else {
+                    input.classList.add("error");
+                    var errorCode = ag_iti_cell.getValidationError();
+                    errorMsg.innerHTML = errorMap[errorCode];
+                    errorMsg.classList.remove("hide");
+                    $('[name=mobile]').val('');
+                }
+            }
+        });
+
+        input.addEventListener('change', reset);
+        input.addEventListener('keyup', reset);
+
         let form = $('#registrationForm');
         form.validate({
             rules: {
@@ -29,7 +65,9 @@
                 },
                 'mobile_#': {
                     required: true,
-                    checkcellnum: true,
+                },
+                'mobile': {
+                    required: true,
                 },
                 email: {
                     required: true,
@@ -56,47 +94,30 @@
                     checkdigit: "Need atleast 1 digit",
                     checkspecialchr: "Need atleast 1 special character"
                 },
-                'mobile_#': {
-            checkcellnum: "Please enter a valid value. (03001234567)"
-        }
-    },
-        errorElement: 'small',
-            errorClass: 'help-block text-red',
+                'mobile': ", please enter a valid value."
+            },
+            errorElement: 'span',
+            errorClass: 'error help-block text-red',
+            ignore: [],
             submitHandler: function (form) {
-            form.submit();
-        },
-        invalidHandler: function (event, validator) {
-            // 'this' refers to the form
-            const errors = validator.numberOfInvalids();
-            if (errors) {
-                let error_tag = $('div.error.text-red.invalid-feedback');
-                error_tag.hide();
-                const message = errors === 1
-                    ? 'You missed 1 field. It has been highlighted'
-                    : 'You missed ' + errors + ' fields. They have been highlighted';
-                $('div.error.text-red.invalid-feedback strong').html(message);
-                error_tag.show();
-            } else {
-                $('#submit-block').show();
-                $('div.error.text-red.invalid-feedback').hide();
+                form.submit();
+            },
+            invalidHandler: function (event, validator) {
+                // 'this' refers to the form
+                const errors = validator.numberOfInvalids();
+                if (errors) {
+                    let error_tag = $('div.error.text-red.invalid-feedback');
+                    error_tag.hide();
+                    const message = errors === 1
+                        ? 'You missed 1 field. It has been highlighted'
+                        : 'You missed ' + errors + ' fields. They have been highlighted';
+                    $('div.error.text-red.invalid-feedback strong').html(message);
+                    error_tag.show();
+                } else {
+                    $('#submit-block').show();
+                    $('div.error.text-red.invalid-feedback').hide();
+                }
             }
-        }
-    });
-        let mobile_num = $("#cell");
-        var ag_iti_cell = window.intlTelInput(document.querySelector('#cell'), {
-            // any initialisation options go here
-            allowDropdown: false,
-            onlyCountries: ['pk'],
-            // preventInvalidNumbers: true,
-            separateDialCode: true,
-            numberType: "MOBILE",
-            utilsScript: "/../../plugins/intl-tel-input/js/utils.js"
-        });
-
-        mobile_num.change(function () {
-            let data = ag_iti_cell.getNumber().split('+92');
-            let value = "+92-" + data[1];
-            $("input[name='mobile']").val(value);
         });
     });
 })(jQuery);
