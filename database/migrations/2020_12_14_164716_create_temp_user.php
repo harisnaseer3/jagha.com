9a1b5781c503db894e1364e199b79e38f4ba46ab
fatimarida <ridafatima477@gmail.com>
2020-12-14 18:22:42 +0500

    added unverified user delete cron job after an hour

---
+++ database/migrations/2020_12_14_164716_create_temp_user.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTempUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('temp_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->dateTime('expire_at');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        DB::statement('ALTER TABLE `temp_users`
+            CHANGE `created_at` `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+            CHANGE `updated_at` `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL DEFAULT NULL');
+}
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temp_user');
+    }
+}
