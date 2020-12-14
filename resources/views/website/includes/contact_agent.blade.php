240eeaf5a79ef67b4066a30f0a76de51d8b30dbe
aniqakhan755 <aniqk755@gmail.com>
2020-12-14 11:13:16 -0500

    Modified Labels in Contact Agent panel in detail page

--- resources/views/website/includes/contact_agent.blade.php
+++ resources/views/website/includes/contact_agent.blade.php
@@ -4,12 +4,12 @@
     <div class="m-border"></div>
     <div class="Subscribe-box" aria-label="Agency contact form">
         {{ Form::open(['route'=>['contact'],'method' => 'post','role' => 'form', 'id'=> 'email-contact-form', 'role' => 'form']) }}
-        <div><label class="mt-2">Name<span style="color:red">*</span></label></div>
+        <div><label class="mt-2">Your Name<span style="color:red">*</span></label></div>
         {{ Form::text('name', \Illuminate\Support\Facades\Auth::check()? \Illuminate\Support\Facades\Auth::user()->name:null, array_merge(['required'=>'true','class' => 'form-control form-control-sm user-name' , 'aria-describedby' => 'name' . '-error', 'aria-invalid' => 'false', 'placeholder'=>'Name'])) }}
-        <div><label class="mt-2">Email<span style="color:red">*</span></label></div>
+        <div><label class="mt-2">Your Email<span style="color:red">*</span></label></div>
         {{ Form::email('email', \Illuminate\Support\Facades\Auth::check()? \Illuminate\Support\Facades\Auth::user()->email:null, array_merge(['required'=>'true','class' => 'form-control form-control-sm user-email', 'aria-describedby' => 'email' . '-error', 'aria-invalid' => 'false', 'placeholder'=> "name@domain.com"])) }}
 
-        <div><label class="mt-2">Mobile #<span style="color:red">*</span></label></div>
+        <div><label class="mt-2">Your Mobile #<span style="color:red">*</span></label></div>
         {{ Form::tel('phone_#', \Illuminate\Support\Facades\Auth::check()? \Illuminate\Support\Facades\Auth::user()->cell:null, array_merge(['required'=>'true', 'id'=>'cell', 'class' => 'form-control form-control-sm', 'aria-describedby' => 'phone' . '-error', 'aria-invalid' => 'false'])) }}
         <span id="valid-msg" class="hide validated mt-2">✓ Valid</span>
         <span id="error-msg" class="hide error mt-2"></span>
