6d51aa1f97c5deb1146b9fb50ff91b0c64cca898
aniqakhan755 <aniqk755@gmail.com>
2020-12-22 07:47:52 -0500

    Fixed Issues

--- resources/views/components/form/file.blade.php
+++ resources/views/components/form/file.blade.php
@@ -8,7 +8,7 @@
     </label>
     <div class="col-sm-8 col-md-5 col-lg-6 col-xl-5">
         <div class="custom-file">
-        {{ Form::file($name, array_merge(['aria-describedby' => $name . '-error','class'=>'custom-file-input' ,'aria-invalid' => 'false', 'id' => str_replace('[]', '', $name), 'multiple' => empty($attributes['multiple']) ? '' : 'multiple'])) }}
+        {{ Form::file($name, array_merge(['aria-describedby' => $name . '-error','class'=>'custom-file-input form-control form-control-sm' ,'aria-invalid' => 'false', 'id' => str_replace('[]', '', $name), 'multiple' => empty($attributes['multiple']) ? '' : 'multiple'])) }}
             <label class="custom-file-label" for="customFile">Choose file</label>
         </div>
         <ul class='{{str_replace('[]', '', $name)}}-files mt-2'></ul>
@@ -31,7 +31,7 @@
     </div>
 
     @if(!empty($attributes['data-default']))
-        <div class="offset-xs-4 col-xs-8 offset-sm-4 col-sm-8 offset-md-0 col-md-4 col-lg-4 col-xl-5 text-muted data-default-line-height my-auto" >
+        <div class="offset-xs-4 col-xs-8 offset-sm-4 col-sm-8 offset-md-0 col-md-4 col-lg-4 col-xl-5 text-muted data-default-line-height my-sm-auto" >
             {{ $attributes['data-default'] }}
         </div>
     @endif
