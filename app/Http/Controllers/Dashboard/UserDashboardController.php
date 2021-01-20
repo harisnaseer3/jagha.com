5b06ece4cda182322c007e1877c056e9ef010a4b
fatimarida <ridafatima477@gmail.com>
2021-01-20 23:38:29 +0500

    added property count on user dashboard counter

--- app/Http/Controllers/Dashboard/UserDashboardController.php
+++ app/Http/Controllers/Dashboard/UserDashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\PropertyBackendListingController;
 use App\Models\Agency;
 use App\Models\Dashboard\User;
 use App\Models\Property;
@@ -29,20 +30,19 @@ class UserDashboardController extends Controller
      */
     public function index()
     {
-//        dd(Auth::user()->roles[0]->name);
-
-        $user = (new User)->where('id', '=', Auth::user()->getAuthIdentifier())->first();
+        $user = Auth::user()->getAuthIdentifier();
         $agencies = count((new Agency)->where('status', '=', 'verified')->whereIn('id', DB::table('agency_users')->select('agency_id')->where('user_id', '=', Auth::user()->getAuthIdentifier())->pluck('agency_id')->toArray())->get());
 
-        $active_properties = count((new Property)->where('user_id', '=', Auth::user()->getAuthIdentifier())->where('status', '=', 'active')->get());
-        $deleted_properties = count((new Property)->where('user_id', '=', Auth::user()->getAuthIdentifier())->where('status', '=', 'deleted')->get());
-        $pending_properties = count((new Property)->where('user_id', '=', Auth::user()->getAuthIdentifier())->where('status', '=', 'pending')->get());
+        $active_properties = (new PropertyBackendListingController)->userListingsCount('active', $user, 'all');
+        $deleted_properties = (new PropertyBackendListingController)->userListingsCount('deleted', $user, 'all');
+        $pending_properties = (new PropertyBackendListingController)->userListingsCount('pending', $user, 'all');
+
         return view('website.user-dashboard.dashboard',
             [
-                'user' => $user,
+                'user' => (new User)->where('id', '=', Auth::user()->getAuthIdentifier())->first(),
                 'agencies' => $agencies,
-                'sale' => (new Property)->where('user_id', '=', Auth::user()->getAuthIdentifier())->where('purpose', '=', 'Sale')->orderBy('id','desc')->get(),
-                'rent' => (new Property)->where('user_id', '=', Auth::user()->getAuthIdentifier())->where('purpose', '=', 'Rent')->orderBy('id','desc')->get(),
+                'sale' => (new Property)->where('user_id', '=', Auth::user()->getAuthIdentifier())->where('purpose', '=', 'Sale')->orderBy('id', 'desc')->get(),
+                'rent' => (new Property)->where('user_id', '=', Auth::user()->getAuthIdentifier())->where('purpose', '=', 'Rent')->orderBy('id', 'desc')->get(),
                 'active_properties' => $active_properties,
                 'pending_properties' => $pending_properties,
                 'deleted_properties' => $deleted_properties,
