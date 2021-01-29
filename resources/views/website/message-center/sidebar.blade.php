dbfe3d47dce2099c863b23ef49d4f9e78681f9f1
aniqakhan755 <aniqk755@gmail.com>
2021-01-29 12:55:15 -0500

    Optimized Support Page and fixed Labels

--- resources/views/website/message-center/sidebar.blade.php
+++ resources/views/website/message-center/sidebar.blade.php
@@ -2,9 +2,9 @@
     <div class="card-header theme-grey text-white text-capitalize">Message Center</div>
     <ul class="list-group">
         <li class="list-group-item  {{ $current_route_name === 'message-center.inbox' ? 'active' : '' }}"><a
-                href="{{ route('message-center.inbox') }}" class="{{ $current_route_name === 'message-center.inbox' ? 'text-white' : '' }}">Inbox</a></li>
-            <li class="list-group-item {{ $current_route_name === 'message-center.sent' ? 'active' : '' }}"><a
-                    href="{{ route('message-center.sent') }}" class="{{ $current_route_name === 'message-center.sent' ? 'text-white' : '' }}">Support & Inquiry Mails</a></li>
+                href="{{ route('message-center.inbox') }}" class="{{ $current_route_name === 'message-center.inbox' ? 'text-white' : '' }}">Customer Inquiries</a></li>
+        <li class="list-group-item {{ $current_route_name === 'message-center.sent' ? 'active' : '' }}"><a
+                href="{{ route('message-center.sent') }}" class="{{ $current_route_name === 'message-center.sent' ? 'text-white' : '' }}">My Inquiries & Support Requests</a></li>
         <li class="list-group-item {{ $current_route_name === 'message-center.notifications' ? 'active' : '' }}"><a
                 href="{{ route('message-center.notifications') }}" class="{{ $current_route_name === 'message-center.notifications' ? 'text-white' : '' }}">Notifications</a></li>
 
