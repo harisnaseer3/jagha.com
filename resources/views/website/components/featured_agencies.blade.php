f1f93e1187dae061088a020c254eb67ada0eb2f7
fatimarida <ridafatima477@gmail.com>
2021-02-03 23:11:01 +0500

    remove local host dummy loog

--- resources/views/website/components/featured_agencies.blade.php
+++ resources/views/website/components/featured_agencies.blade.php
@@ -9,7 +9,7 @@
                                                ])}}">
                 <img src="{{asset('thumbnails/agency_logos/'.explode('.',$agency->logo)[0].'-100x100'.'.webp')}}" alt="{{strtoupper($agency->title)}}" width="50%" height="50%"
                      class="img-fluid"
-                     title="{{strtoupper($agency->title)}}" onerror="this.src='http://localhost/img/logo/dummy-logo.png'">
+                     title="{{strtoupper($agency->title)}}" onerror="this.src={{asset('img/logo/dummy-logo.png')}}">
             </a>
         @else
             <a href="{{route('agents.ads.listing',
@@ -17,7 +17,7 @@
                                                'slug'=>\Illuminate\Support\Str::slug($agency->title),
                                                'agency'=> $agency->id ,
                                                ])}}">
-                <img src="http://localhost/img/logo/dummy-logo.png" alt="{{strtoupper($agency->title)}}" width="50%" height="50%" class="img-fluid" title="{{strtoupper($agency->title)}}">
+                <img src="{{asset('img/logo/dummy-logo.png')}}" alt="{{strtoupper($agency->title)}}" width="50%" height="50%" class="img-fluid" title="{{strtoupper($agency->title)}}">
             </a>
         @endif
         <h2 class="agency-name mt-3 text-transform d-none">{{$agency->title}}</h2>
--- resources/views/website/components/key_agencies.blade.php
+++ resources/views/website/components/key_agencies.blade.php
@@ -16,8 +16,8 @@
                                                'slug'=>\Illuminate\Support\Str::slug($agency->title),
                                                'agency'=> $agency->id ,
                                                ])}}">
-                            <img src="{{$agency->user_id !== 1?asset('thumbnails/agency_logos/'.explode('.',$agency->logo)[0].'-100x100'.'.webp'):asset('storage/agency_logos/'.'img256by256_1588397752.jpg')}}" loading="lazy" alt="{{$agency->title}}"
-                                 class="img-fluid featured-agency-image" onerror="this.src='http://localhost/img/logo/dummy-logo.png'" style="height:53px; width: 60px ;">
+                            <img src="{{$agency->user_id !== 1?asset('thumbnails/agency_logos/'.explode('.',$agency->logo)[0].'-100x100'.'.webp'):asset('img/logo/dummy-logo.png')}}" loading="lazy" alt="{{$agency->title}}"
+                                 class="img-fluid featured-agency-image" onerror="this.src='{{asset('img/logo/dummy-logo.png')}}'" style="height:53px; width: 60px ;">
                         </a>
                     </div>
                     <div class="col-8">
