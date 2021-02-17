73fac6d7cf5a596c2916401145171141e2bbbc50
fatimarida <ridafatima477@gmail.com>
2021-02-17 16:12:04 +0500

    update cron job

--- app/Http/Controllers/CronJobController.php
+++ app/Http/Controllers/CronJobController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\SendNotificationOnPropertyUpdate;
 use App\Models\Dashboard\City;
 use App\Models\Dashboard\Location;
 use App\Models\Dashboard\User;
@@ -10,6 +11,7 @@ use App\Models\Property;
 use App\Models\TempImage;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class CronJobController extends Controller
@@ -32,19 +34,25 @@ class CronJobController extends Controller
     private function updatePropertyStatus()
     {
         //add  query to compare today's and expiry date of property
-        $columns = (new Property)->whereDate('expired_at', '<=', Carbon::now()->toDateTimeString())->get();
+        $columns = (new Property)->where('status', '=', 'active')->whereDate('expired_at', '<=', Carbon::now()->toDateTimeString())->limit(50)->get();
         foreach ($columns as $column) {
             (new CountTableController())->_delete_in_status_purpose_table($column, $column->status);
             $city = (new City)->select('id', 'name')->where('id', '=', $column->city_id)->first();
-            $location_obj = (new Location)->select('id', 'name')->where('id', '=', $column->location_id)->first();
-            $location = ['location_id' => $location_obj->id, 'location_name' => $location_obj->name];
+            $location = (new Location)->select('id', 'name')->where('id', '=', $column->location_id)->first();
 
-
-            $column->status = 'expired';
-            $column->save();
+            $column->update([
+                'status' => 'expired',
+                'activated_at' => null,
+                'expired_at' => date('Y-m-d H:i:s'),
+            ]);
             (new CountTableController())->_on_deletion_insertion_in_count_tables($city, $location, $column);
             (new CountTableController)->_insert_in_status_purpose_table($column);
+            $this->dispatch(new SendNotificationOnPropertyUpdate($column));
+            if (Auth::guard('admin')->user()) {
+                (new PropertyLogController())->store($column);
+            }
         }
+
         return true;
     }
 
