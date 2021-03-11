0f63a3c3f8171eadee568f1cea75455ca61ef91b
fatimarida <ridafatima477@gmail.com>
2021-03-12 01:02:49 +0500

    added job for DB tables update after adding agency owner

--- app/Http/Controllers/AgencyOwnerController.php
+++ app/Http/Controllers/AgencyOwnerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\UpdateDBTables;
 use App\Models\Agency;
 use App\Models\AgencyLog;
 use App\Models\Dashboard\User;
@@ -16,14 +17,14 @@ class AgencyOwnerController extends Controller
     public function edit(Agency $agency)
     {
         if ($agency->user_id !== 1) {
-            return redirect()->route('agencies.listings', [
+            return redirect()->route('admin.agencies.listings', [
                 'status' => 'verified_agencies',
                 'purpose' => 'all',
-                'user' => Auth::user()->getAuthIdentifier(),
+                'user' => Auth::guard('admin')->user()->getAuthIdentifier(),
                 'sort' => 'id',
-                'order' => 'desc',
-                'page' => 10,
-            ])->with('error', 'Something Went Wrong');
+                'order' => 'asc',
+                'page' => 200,
+            ])->with('error', 'Something went wrong.');
         }
         $city = $agency->city->name;
         $agency->city = $city;
@@ -52,83 +53,8 @@ class AgencyOwnerController extends Controller
         $user = User::getUserByEmail($request->email);
         if ($user) {
 
-            DB::table('agencies')
-                ->where('id', '=', $agency->id)
-                ->update(
-                    ['user_id' => $user->id]
-                );
-            DB::table('agency_users')
-                ->where('agency_id', '=', $agency->id)
-                ->update(
-                    ['user_id' => $user->id]
-                );
-            DB::table('properties')
-                ->where('agency_id', '=', $agency->id)
-                ->update(
-                    ['user_id' => $user->id]
-                );
-            DB::table('property_count_by_user')
-                ->updateOrInsert(
-                    ['agency_id' => $agency->id],
-                    ['user_id' => $user->id]
-                );
-            $sale_active_count = DB::table('property_count_by_user')->select('agency_count')
-                ->where('user_id', '=', $user->id)
-                ->where('agency_id', '=', $agency->id)
-                ->where('property_purpose', '=', 'sale')
-                ->where('property_status', '=', 'active')->first();
+            $this->dispatch(new UpdateDBTables($agency, $user, Auth::guard('admin')->user()->getAuthIdentifier(), Auth::guard('admin')->user()->name));
 
-
-            $rent_active_count = DB::table('property_count_by_user')->select('agency_count')
-                ->where('user_id', '=', $user->id)
-                ->where('agency_id', '=', $agency->id)
-                ->where('property_purpose', '=', 'rent')
-                ->where('property_status', '=', 'active')->first();
-            if ($sale_active_count) {
-
-                $sale_active_count = $sale_active_count->agency_count;
-
-                DB::table('property_count_by_status_and_purposes')->select('property_count')
-                    ->where('user_id', '=', 1)
-                    ->where('property_purpose', '=', 'sale')
-                    ->where('property_status', '=', 'active')
-                    ->where('listing_type', '=', 'basic_listing')
-                    ->where('property_count', '>=', $sale_active_count)
-                    ->decrement('property_count', $sale_active_count);
-
-                DB::table('property_count_by_status_and_purposes')
-                    ->where('user_id', '=', $user->id)
-                    ->where('property_purpose', '=', 'sale')
-                    ->where('property_status', '=', 'active')
-                    ->where('listing_type', '=', 'basic_listing')
-                    ->increment('property_count', $sale_active_count);
-            }
-            if ($rent_active_count) {
-                $rent_active_count = $rent_active_count->agency_count;
-                DB::table('property_count_by_status_and_purposes')
-                    ->where('user_id', '=', 1)
-                    ->where('property_purpose', '=', 'rent')
-                    ->where('property_status', '=', 'active')
-                    ->where('listing_type', '=', 'basic_listing')
-                    ->where('property_count', '>=', $rent_active_count)
-                    ->decrement('property_count', $rent_active_count);
-
-                DB::table('property_count_by_status_and_purposes')
-                    ->where('user_id', '=', $user->id)
-                    ->where('property_purpose', '=', 'rent')
-                    ->where('property_status', '=', 'active')
-                    ->where('listing_type', '=', 'basic_listing')
-                    ->increment('property_count', $rent_active_count);
-            }
-
-            (new AgencyLog)->create([
-                'admin_id' => Auth::guard('admin')->user()->getAuthIdentifier(),
-                'admin_name' => Auth::guard('admin')->user()->name,
-                'agency_id' => $agency->id,
-                'agency_title' => $agency->title,
-                'status' => 'Add Agency Owner',
-                'rejection_reason' => $agency->rejection_reason,
-            ]);
             return redirect()->route('admin.agencies.listings', [
                 'status' => 'verified_agencies',
                 'purpose' => 'all',
