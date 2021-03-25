a0fc7a09c76ea2f05ec36acfda14b82f33a70f04
fatimarida <ridafatima477@gmail.com>
2021-03-25 19:23:16 +0500

    redirect to custom error page

---
+++ errors/404.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{csrf_token()}}"/>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-170072080-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-170072080-1');
+    </script>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+    <!-- External CSS libraries -->
+    <link rel="stylesheet" type="text/css" href="/website/css/bootstrap.min.css" async defer>
+    <link rel='stylesheet' id='mvp-style-css' href="/website/css/menustyle.min.css" type='text/css' media='all' async defer/>
+    <!-- <link crossorigin="anonymous" rel='stylesheet' id='mvp-fonts-css' href='//fonts.googleapis.com/css?family=Oswald%3A400%2C700%7CLato%3A400%2C700%7CWork+Sans%3A900%7CMontserrat%3A400%2C700%7COpen+Sans%3A800%7CPlayfair+Display%3A400%2C700%2C900%7CQuicksand%7CRaleway%3A200%2C400%2C700%7CRoboto+Slab%3A400%2C700%7CWork+Sans%3A100%2C200%2C300%2C400%2C500%2C600%2C700%2C800%2C900%7CMontserrat%3A100%2C200%2C300%2C400%2C500%2C600%2C700%2C800%2C900%7CWork+Sans%3A100%2C200%2C300%2C400%2C500%2C600%2C700%2C800%2C900%7CLato%3A100%2C200%2C300%2C400%2C500%2C600%2C700%2C800%2C900%7CMontserrat%3A100%2C200%2C300%2C400%2C500%2C600%2C700%2C800%2C900%26subset%3Dlatin%2Clatin-ext%2Ccyrillic%2Ccyrillic-ext%2Cgreek-ext%2Cgreek%2Cvietnamese&display=swap'type='text/css' media='all'/> -->
+    <link rel="stylesheet" type="text/css" href="/plugins/fontawesome-pro-5.12.0/css/all.min.css" async defer>
+    <link rel="stylesheet" type="text/css" href="/website/css/slick.min.css" async defer>
+    <link rel="stylesheet" href="/plugins/select2/css/select2.min.css" async defer>
+    <!-- Favicon icon -->
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+
+    <link rel="stylesheet" type="text/css" href="/website/fonts/font-awesome/css/font-awesome.min.css" async defer>
+
+    <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" async defer>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700" rel="stylesheet" async defer>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<!--[if eq IE 10]><link rel="stylesheet" type="text/css" href="/website/css/ie10-viewport-bug-workaround.css"><![endif]-->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="/website/js/html5shiv.min.js"></script>
+<script src="/website/js/respond.min.js"></script>
+<![endif]-->
+
+
+    <!-- Custom stylesheet -->
+    <link rel="stylesheet" type="text/css" href="/website/css/style.min.css" async defer>
+    <link rel="stylesheet" type="text/css" href="/website/css/skins/default.css" id="style_sheet" async defer>
+    <link rel="stylesheet" type="text/css" href="/website/css/custom.min.css">
+
+    <style>
+
+        .field-icon {
+            float: right;
+            margin-left: -25px;
+
+            position: relative;
+            z-index: 2;
+            margin-right: 8px;
+            font-size: 1.2rem;
+
+        }
+    </style>
+</head>
+<body>
+
+<div class="pages-404-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="pages-404-inner">
+                    <h1>Oops... Page Not Found !</h1>
+                    <p class="lead">The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+                    <a href="https://property.aboutpakistan.com" class="border-thn">Go to Home Page</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="fixed-bottom p-2">
+    <div class="toast bg-dark w-100 mw-100 color-white" role="alert" style="display: none">
+        <div class="toast-body p-2 d-flex flex-column">
+            <div class="color-white">
+                <span class="mr-auto color-white" style="line-height: 1.5rem">
+                We use cookies for a number of reasons, such as keeping our site reliable and secure, providing social media features and analyzing how our site is being used by end user. By selecting "Accept" option or continuing to browse site, you consent to the usage of these cookies on your device.
+                </span>
+                <span class="float-right pt-2 mt-2"><a type="button ml-auto" class="btn btn-sm theme-blue text-white" id="btnAccept">Accept</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="/website/js/jquery-2.2.0.min.js"></script>
+<script src="/website/js/bootstrap.min.js"></script>
+<script src="/website/js/wordpress.minify.js" defer async></script>
+<script src="/website/js/bootstrap.bundle.min.js" defer></script>
+
+</body>
+</html>
