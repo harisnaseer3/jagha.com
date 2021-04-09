44c6714ebe89d634058ce30c9315d75b541ccaff
fatimarida <ridafatima477@gmail.com>
2021-04-09 13:09:39 +0500

    add facepost job

---
+++ app/Jobs/FacebookPost.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Property;
+use Facebook\Exceptions\FacebookSDKException;
+use Facebook\Facebook;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Str;
+
+class FacebookPost implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    protected $property;
+
+    public function __construct(Property $property)
+    {
+        $this->property = $property;
+    }
+
+    public function handle()
+    {
+        $token = 'EAADU6kME7ZCIBAFAQjktiIX2qFpLhBRRwCmxfZBCuIXTaeXCfNMGLtOiVD9PmY0sEEZBDv1MYuZBEFlGhLblfdlYnt2ZB5fXdmeX7MENyGD3NcQhNF8Scp2crom4n5vMp9qIlZBZCz5CsjFqZCQZBRaufQrpsWCysxMGljNxAyWFEhALrflsAJREbwAQdlu96fHgZD';
+
+        $fb = new Facebook([
+            'app_id' => '234102611832818',
+            'app_secret' => '17c51b4cd6934b727435d804658248ed',
+            'default_graph_version' => 'v5.0',
+        ]);
+//        $absolute_image_path =  asset('thumbnails/properties/'.explode('.',$property->image)[0].'-450x350.webp');
+//        dd($fb->fileToUpload(base_path('thumbnails\properties\71617895243-750x600.webp')));
+//        dd(base_path('thumbnails\properties\71617895243-750x600.png'));
+//
+
+        $dummy_image = base_path('img\logo\dummy-logo.png');
+//
+        $property = $this->property;
+        $absolute_image_path = isset($property->image) ? base_path('thumbnails/properties/' . explode('.', $property->image)[0] . '-450x350.webp') : $dummy_image;
+        $source = $fb->fileToUpload($absolute_image_path);
+        $link = route('properties.show', [
+            'slug' => Str::slug($property->location) . '-' . Str::slug($property->title) . '-' . $property->reference,
+            'property' => $property->id]);
+
+        $linkData = [
+            'link' => $link,
+            'message' => $property->title,
+            'source' => $source
+        ];
+        $page_id = '906497989423481';
+
+        try {
+            $post = $fb->post('/' . $page_id . '/photos', $linkData, $token);
+            $post = $post->getGraphNode()->asArray();
+            print_r('Facebook Post Successfully Created.' . $post);
+
+        } catch (FacebookSDKException $e) {
+            echo 'Graph returned an error: ' . $e->getMessage();
+//            exit;
+        }
+    }
+}
