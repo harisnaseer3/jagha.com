3abb4ebed27fce613343e64073862ae9c0c742f0
fatimarida <ridafatima477@gmail.com>
2021-04-15 01:37:16 +0500

    server push

--- resources/views/website/components/about_pakistan_properties.blade.php
+++ resources/views/website/components/about_pakistan_properties.blade.php
@@ -1,4 +1,4 @@
-<div id="carouselControls" class="carousel slide " data-ride="carousel" data-interval="5000">
+<div id="carouselControls" class="carousel slide " data-ride="carousel" data-interval="3000">
     <div class="carousel-inner property-inner row w-100 mx-auto" role="listbox">
 
         @foreach($featured_properties as $index => $feature_property)
