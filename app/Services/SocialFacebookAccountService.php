aa515c347a16f52cac57351a9fc34894b3f6aea3
fatimarida <ridafatima477@gmail.com>
2021-08-06 20:40:39 +0500

    skipped email check on user fb login

--- app/Services/SocialFacebookAccountService.php
+++ app/Services/SocialFacebookAccountService.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace App\Services;
+
 use App\Models\SocialFacebookAccount;
 use App\Models\Dashboard\User;
 use Carbon\Carbon;
 use Illuminate\Auth\Events\Verified;
 use Laravel\Socialite\Contracts\User as ProviderUser;
 use Illuminate\Support\Facades\File;
+
 class SocialFacebookAccountService
 {
     public function createOrGetUser(ProviderUser $providerUser)
@@ -20,19 +23,29 @@ class SocialFacebookAccountService
                 'provider_user_id' => $providerUser->getId(),
                 'provider' => 'facebook'
             ]);
-            $user = User::whereEmail($providerUser->getEmail())->first();
-            if (!$user) {
-                $user = User::create([
-                    'email' => $providerUser->getEmail(),
-                    'name' => $providerUser->getName(),
-                    'password' => md5(rand(1,10000)),
-                ]);
-                event(new Verified($user));
-            }
+            $email = $providerUser->getEmail();
+            $user = '';
+            if (isset($email)) {
+                $user = User::whereEmail($providerUser->getEmail())->first();
+                if (!$user) {
+                    $user = User::create([
+                        'email' => $providerUser->getEmail(),
+                        'name' => $providerUser->getName(),
+                        'password' => md5(rand(1, 10000)),
+                    ]);
+                    event(new Verified($user));
+                }
 
+            } else {
+                $user = User::create([
+                    'name' => $providerUser->getName(),
+                    'password' => md5(rand(1, 10000)),
+                ]);
+            }
             $account->user()->associate($user);
             $account->save();
             return $user;
+
         }
     }
 }
