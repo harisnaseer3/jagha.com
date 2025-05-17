948bf7f7e1befad523a494a41391f413a3819992
harisnaseer3 <harisnaseer3@gmail.com>
2025-05-17 11:58:36 +0500

    changes latest

--- config/mail.php
+++ config/mail.php
@@ -83,7 +83,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'About Pakistan Properties'),
+        'name' => env('MAIL_FROM_NAME', 'Jagha.com'),
     ],
 
     /*
